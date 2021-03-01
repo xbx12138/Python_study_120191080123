16701a5a422e31575f1f9f9710359121a710cb0a
xbx12138 <1455051433@qq.com>
2021-03-01 17:19:40 +0800

    Init

---
+++ exercise/Helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:Helloworld.py
+# author:Asus
+# datetime:2021/3/1 16:57
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+print("Hello，world！")
