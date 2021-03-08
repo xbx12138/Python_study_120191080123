c8ff6a9af1f2aae27ff3ddef5df10d981e1ecd3a
xbx12138 <1455051433@qq.com>
2021-03-08 15:11:32 +0800

    Create do_list2.py

---
+++ exercise/第二周/do_list2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:do_list2.py
+# author:Asus
+# datetime:2021/3/8 15:01
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+
+import random
+
+score = [random.randint(60, 100) for _ in range(10)]
+
+Max1 = max(score);
+Min1 = min(score);
+Sum = 0
+for i in score:
+    Sum += i
+avg = Sum / len(score)
+print(f'{score}\n最高分：{Max1}\n最低分：{Min1}\n总分：{Sum}\n平均分：{avg}')
