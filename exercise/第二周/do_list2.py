7c9f4340b630e16632ed8362db6618b81252cea0
xbx12138 <1455051433@qq.com>
2021-03-08 15:46:24 +0800

    Update do_list2.py

--- exercise/第二周/do_list2.py
+++ exercise/第二周/do_list2.py
@@ -16,8 +16,7 @@ score = [random.randint(60, 100) for _ in range(10)]
 
 Max1 = max(score);
 Min1 = min(score);
-Sum = 0
-for i in score:
-    Sum += i
+Sum = sum(score)
+
 avg = Sum / len(score)
 print(f'{score}\n最高分：{Max1}\n最低分：{Min1}\n总分：{Sum}\n平均分：{avg}')
