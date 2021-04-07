e20e4d551fb46812403ab071317d4ad1ef1cd64c
xbx12138 <1455051433@qq.com>
2021-04-07 16:48:07 +0800

    commit

--- homework/homework3/3.py
+++ homework/homework3/3.py
@@ -9,13 +9,21 @@
 3 编写一个程序，读取文件中保存的10个学生成绩名单信息(学号,姓名, Python课程分数);
  然后按照分数从高到低进行排序输出
 '''
+
+
 # import module your need
+
+def sort(nums):  # 冒泡排序
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - i - 1):
+            if nums[j][2] < nums[j + 1][2]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
 
 
 if __name__ == '__main__':
     with open('./3/score.txt', 'r', encoding='utf-8') as fr:
         Head = fr.readline().strip().split('    ')
-        print(Head)
         # 将信息存储在二维列表
         a = []
         for line in fr.readlines():
@@ -26,4 +34,12 @@ if __name__ == '__main__':
         # 将分数转化
         for i in a:
             i[2] = float(i[2])
-        print(a)
+
+        sort(a)
+        for i in Head:
+            print(i, end='    ')
+        for i in a:
+            print('\n')
+            for j in i:
+                print(j, end='    ')
+
---
+++ homework/homework3/6.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:6.py
+# author:Asus
+# datetime:2021/4/7 16:03
+# software: PyCharm
+'''
+6 对一篇英文小说，进行词频统计，输出前20个出现频率最高的单词；
+英文小说"老人与海"
+链接：https://pan.baidu.com/s/1goqK3zF8VBUFuD_2ezfZ7Q 提取码：mv04
+提示： 1 可以先定义一个函数，专门来处理英文文章的读取问题；
+读取时，解决大小写问题，以及标点符号问题（如，将标点符号都替换成空格）；
+'''
+
+
+# import module your need
+
+def getText():
+    with open('./6/《老人与海》[英文版].txt', 'r', encoding='utf-8') as fr:
+        txt = fr.read()
+        txt = txt.lower().replace('\n', ' ')
+        for ch in '!@#$%^&*()_+~`";:,<.>/?\|=-[]{}':
+            txt = txt.replace(ch, ' ')
+        return txt
+
+
+if __name__ == '__main__':
+    words = {}
+    wordlist = []
+    txt = getText()
+    txtlist = txt.split(' ')
+    for word in txtlist:
+        if word in words:
+            words[word] += 1
+        else:
+            words[word] = 1
+    for k, v in words.items():
+        tup = (k, v)
+        wordlist.append(tup)
+    n=len(wordlist)
+    for i in range(1,n):
+        temp=wordlist[i]
+        j=i-1
+        while j>=0 and wordlist[j][1]<temp[1]:
+            wordlist[j+1]=wordlist[j]
+            j-=1
+        wordlist[j+1]=temp
+
+    for i in range(1,21):
+        print(f'{i}: {wordlist[i][0]} 次数：{wordlist[i][1]}')
