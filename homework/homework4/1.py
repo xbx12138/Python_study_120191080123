3f285cb6b962bbfe54eeed3266f372f63bd0da59
xbx12138 <1455051433@qq.com>
2021-04-19 13:03:06 +0800

    commit

---
+++ homework/homework4/1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:1.py
+# author:Asus
+# datetime:2021/4/18 23:33
+# software: PyCharm
+'''
+1 定义一个10个元素的列表，通过列表自带的函数，实现元素在尾部插入和头部插入并记录程序运行的时间；
+用deque来实现，同样记录程序所耗费的时间；输出这2个时间的差值；
+    提示：列表原生的函数实现头部插入数据：list.insert(0, v)；list.append（2）)
+'''
+# import module your need
