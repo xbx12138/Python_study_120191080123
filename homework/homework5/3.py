697b5f8220ab77c38e8eca868703e220684fb350
xbx12138 <1455051433@qq.com>
2021-04-24 12:41:02 +0800

    commit

---
+++ homework/homework5/3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:3.py
+# author:Asus
+# datetime:2021/4/24 12:35
+# software: PyCharm
+'''
+3  编写一个装饰器，为多个函数加上认证的功能（必须输入用户的账号密码，才能调用这个函数）
+'''
+# import module your need
