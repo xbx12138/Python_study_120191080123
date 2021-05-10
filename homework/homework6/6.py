e5e30c4a1946b92d32748e75d91416174ee71be7
xbx12138 <1455051433@qq.com>
2021-05-10 17:21:16 +0800

    commit

--- homework/homework6/6.py
+++ homework/homework6/6.py
@@ -20,57 +20,74 @@ class Student:
     Students = {}
 
     def add_stu(self, id_stu, name, classes, score):
-        self.Students[id_stu:[name, classes, score]]
+        self.Students[id_stu] = [name, classes, score]
+        print('添加成功！')
 
     def del_stu(self, id_stu):
         self.Students.pop(id_stu)
+        print('删除成功！')
 
     def update_stu(self, id_stu, name, classes, score):
-        self.Students[id_stu:[name, classes, score]]
+        self.Students[id_stu] = [name, classes, score]
+        print('修改成功！')
 
     def search_stu(self, id_stu):
-        print(f'学号：{id_stu}\n姓名：{self.Students[id_stu][0]}\n'
-              f'班级：{self.Students[id_stu][1]}\nPython成绩：{self.Students[id_stu][2]}')
+        if id_stu in self.Students:
+            print(f'学号：{id_stu}\n姓名：{self.Students[id_stu][0]}\n'
+                  f'班级：{self.Students[id_stu][1]}\nPython成绩：{self.Students[id_stu][2]}')
+        else:
+            print('不存在此学生！')
 
     def save_stu(self):
-        with open('.6/json_stu', 'w') as f:
+        with open('./6/json_stu.json', 'w', encoding='utf-8') as f:
             json.dump(self.Students, f)
+        print('保存成功！')
 
     def load_stu(self):
-        with open('.6/json_stu') as f:
-            self.Students=json.load(f)
+        with open('./6/json_stu.json', 'r', encoding='utf-8') as f:
+            self.Students = json.load(f)
+        self.getInfo()
+
+    def getInfo(self):
+        print('学号        姓名        班级       python成绩\n——————————————————————————————————————————————\n')
+        for k, v in self.Students.items():
+            print(f'{k}      {v[0]}        {v[1]}        {v[2]}')
 
 
 if __name__ == '__main__':
     stu = Student()
     print('学生Python成绩管理系统\n1.增加；\n2.删除；\n3.修改；\n4.查询；\n5.读档；\n6.保存。\n    0.退出')
-    flag=int(input('输入功能序号'))
-    while flag==0:
-        if flag==1:
+    flag = int(input('输入功能序号'))
+    while flag != 0:
+        if flag == 1:
             print('增加信息：')
-            id_stu=input('输入学号：')
-            name=input('输入姓名')
-            classes=input('输入班级：')
-            score=input('输入python成绩：')
-            stu.add_stu(id_stu,name,classes,score)
-        elif flag==2:
-            print('删除信息：')
-            id_stu=input('输入需删除学生的学号:')
-            stu.del_stu(id_stu)
-        elif flag==3:
-            print('修改信息：')
-            id_stu=input('输入需修改信息学生的学号：')
-            print('请输入修改后的信息：')
-            name = input('输入姓名')
+            id_stu = input('输入学号：')
+            name = input('输入姓名：')
             classes = input('输入班级：')
             score = input('输入python成绩：')
-            stu.update_stu(id_stu,name,classes,score)
-        elif flag==4:
+            stu.add_stu(id_stu, name, classes, score)
+        elif flag == 2:
+            print('删除信息：')
+            id_stu = input('输入需删除学生的学号:')
+            stu.del_stu(id_stu)
+        elif flag == 3:
+            print('修改信息：')
+            id_stu = input('输入需修改信息学生的学号：')
+            print('请输入修改后的信息：')
+            name = input('输入姓名：')
+            classes = input('输入班级：')
+            score = input('输入python成绩：')
+            stu.update_stu(id_stu, name, classes, score)
+        elif flag == 4:
             print('查询信息：')
             id_stu = input('输入需查询信息学生的学号：')
             stu.search_stu(id_stu)
-        elif flag==5:
+        elif flag == 5:
+            print('读档')
+            stu.load_stu()
+        elif flag == 6:
             print('保存')
             stu.save_stu()
-        flag=int(input('输入功能序号'))
 
+        print('学生Python成绩管理系统\n1.增加；\n2.删除；\n3.修改；\n4.查询；\n5.读档；\n6.保存。\n    0.退出')
+        flag = int(input('输入功能序号'))
