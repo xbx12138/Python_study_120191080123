5697b3d8671ab4c92a5cf60f2663ee07ef065d6c
许钧珲 <1455051433@qq.com>
2021-05-22 21:25:54 +0800

    init homework7

---
+++ homework/homework7/1_Regular_expression_extract_url.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:1_Regular_expression_extract_url.py
+# author:Asus
+# datetime:2021/5/22 11:36
+# software: PyCharm
+'''
+1 给定一个文件，请用正则表达式，逐行匹配提取其中的URL链接信息，并保存到另外一个文件中；
+ 提示，文件有1000行，注意控制每次读取的行数；
+'''
+# import module your need
+
+import re
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
