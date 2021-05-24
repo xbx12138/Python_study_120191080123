be93fd374f24609b66ba3d36cad549c5f13c6540
许钧珲 <1455051433@qq.com>
2021-05-24 11:20:20 +0800

    init homework10

---
+++ homework/homework7/2_spider_business.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:2_spider_business.py
+# author:Asus
+# datetime:2021/5/23 9:41
+# software: PyCharm
+'''
+2 给定100个企业网站首页链接地址（用1中给出的URL地址）；请爬取每个页面上，企业介绍的链接地址；
+说明，满足企业介绍网址的条件是， 标题包含：企业介绍，关于我们，企业发展，发展历史，企业概况等关键字的URL地址；
+提示：要用到requests库，BeautifulSoup库
+'''
+# import module your need
+
+
+import re
+
+
+
